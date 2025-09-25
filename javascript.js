42de43b105da75e207174f23d1c01dfd886f0819
aaron-vong <aaronong74@gmail.com>
2025-09-24 23:21:24 -0500

    Finished setting up required functions

---
+++ javascript.js
@@ -0,0 +1,32 @@
+const humanScore = 0;
+const computerScore = 0;
+
+function playGame() {
+
+}
+
+function getComputerChoice() {
+
+
+
+}
+
+function getHumanChoice() {
+
+
+
+}
+
+function playRound(humanChoice, computerChoice){
+
+    // Case Sensitive
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+// Notes:
+// Game will play 5 rounds. playGame function will call playRound to play 5 rounds.
+// Keep track of the score and declare winner at the end
